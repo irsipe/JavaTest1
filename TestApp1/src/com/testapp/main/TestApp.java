dbe39010f4c8e65fb245516c2c18703a294c1bc3
Pedro Diaz Leiva <pdiazl62@gmail.com>
2017-09-22 14:01:51 +0200

    Version de prueba 1

---
+++ TestApp1/src/com/testapp/main/TestApp.java
@@ -0,0 +1,19 @@
+package com.testapp.main;
+
+public class TestApp {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length>0 && args[0].equals("i"))
+		{
+			System.out.println("Welcome to  POO with Java");	
+		}
+		else
+		{
+		System.out.println("Bienvenido al curso POO con Java");
+		}
+	}
+
+}
